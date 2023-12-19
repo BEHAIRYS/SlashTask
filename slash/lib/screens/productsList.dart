23f8e4c18589277a8b31281a3499341d8be3dbab
BEHAIRYS <70611912+BEHAIRYS@users.noreply.github.com>
2023-12-19 23:55:10 +0200

    Added GridView

--- slash/lib/screens/productsList.dart
+++ slash/lib/screens/productsList.dart
@@ -8,7 +8,54 @@ class ProductsListScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: Text('data'),
+      body: Column(
+        children: [
+          Row(
+            children: [
+              Text('Category Image'),
+              Column(
+                children: [
+                  Text('Category Name'),
+                  ElevatedButton.icon(
+                    onPressed: () {},
+                    icon: Icon(Icons.filter),
+                    label: Text('Filters'),
+                  ),
+                ],
+              )
+            ],
+          ),
+          Expanded(
+            child: GridView.builder(
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                childAspectRatio: 2 / 3,
+              ),
+              itemCount: products.length,
+              itemBuilder: (context, index) {
+                return GridTile(
+                  child: Container(
+                    margin: const EdgeInsets.all(8),
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.grey),
+                      borderRadius: BorderRadius.circular(8.0),
+                    ),
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text('Name: ${products[index].name}'),
+
+                        // Add more widgets as needed
+                      ],
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
