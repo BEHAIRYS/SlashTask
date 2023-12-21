6d1602dd820cadb1d59e97cd197eeb971e90b3f8
BEHAIRYS <70611912+BEHAIRYS@users.noreply.github.com>
2023-12-21 21:05:55 +0200

    finished the project

--- slash/lib/screens/productDetails.dart
+++ slash/lib/screens/productDetails.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert' as convert;
+
 import 'package:slash/datastructures/AvailableProperties.dart';
 import 'package:slash/datastructures/Product.dart';
-import 'dart:convert' as convert;
-import 'package:http/http.dart' as http;
 import 'package:slash/datastructures/ProductVariation.dart';
 import 'package:slash/datastructures/PropertyValues.dart';
 import 'package:slash/widgets/Colors.dart';
@@ -11,23 +12,20 @@ import 'package:slash/widgets/Materials.dart';
 import 'package:slash/widgets/Size.dart';
 
 class ProductDetailsScreen extends StatefulWidget {
-  ProductDetailsScreen({super.key, required this.product});
-  Product product;
+  ProductDetailsScreen({Key? key, required this.product}) : super(key: key);
+  final Product product;
 
   @override
-  State<StatefulWidget> createState() {
-    return _ProductDetailsScreenState();
-  }
+  State<StatefulWidget> createState() => _ProductDetailsScreenState();
 }
 
 class _ProductDetailsScreenState extends State<ProductDetailsScreen> {
   ProductVariation? variation;
-  Widget? variations;
   bool isDataInitialized = false;
-  Key _sizeWidgetKey = UniqueKey();
   List<ProductVariation> currentVariations = [];
+
   @override
-  initState() {
+  void initState() {
     super.initState();
     initializeData();
   }
@@ -40,8 +38,38 @@ class _ProductDetailsScreenState extends State<ProductDetailsScreen> {
       variation = widget.product.variations.isNotEmpty
           ? widget.product.variations[0]
           : null;
-      variations = displayVariations();
     });
+  }
+
+  Future<void> fetchProductVariations() async {
+    try {
+      var url = Uri.https(
+          'slash-backend.onrender.com', '/product/${widget.product.id}');
+      var response = await http.get(url);
+
+      if (response.statusCode == 200) {
+        var jsonResponse = convert.jsonDecode(response.body);
+        if (jsonResponse.containsKey('data')) {
+          List<dynamic> data = jsonResponse['data']['variations'];
+          List<dynamic> availableProperties =
+              jsonResponse['data']['avaiableProperties'];
+
+          setState(() {
+            widget.product.variations =
+                data.map((item) => ProductVariation.fromJson(item)).toList();
+            widget.product.availableProperties = availableProperties
+                .map((property) => AvailableProperties.fromJson(property))
+                .toList();
+          });
+        } else {
+          print('Response does not contain data key.');
+        }
+      } else {
+        print('Request failed with status: ${response.statusCode}.');
+      }
+    } catch (error) {
+      print('Error fetching product variations: $error');
+    }
   }
 
   void _changeVariation(String value) {
@@ -55,126 +83,95 @@ class _ProductDetailsScreenState extends State<ProductDetailsScreen> {
       currentVariations.clear();
       currentVariations.addAll(newVariations);
     });
-    // Print information for debugging
-    print(
-        'Selected Color: ${variation?.productPropertiesValues.firstWhere((property) => property.property == 'Color').value}');
-    print(
-        'Sizes for Selected Color: ${currentVariations.map((element) => element.productPropertiesValues.where((property) => property.property == 'Size').map((property) => property.value)).toList()}');
   }
 
-  Widget displayVariations() {
-    Widget? size, color, material;
-    if (variation == null) {
+  Widget _buildColorsWidget() {
+    if (!widget.product.availableProperties
+        .any((property) => property.property == 'Color')) {
       return Container();
     }
-    if (widget.product.availableProperties
-        .any((property) => property.property == 'Color')) {
-      List<String> values = [];
-      widget.product.availableProperties
-          .where((element) => element.property == 'Color')
-          .forEach((property) {
-        values = property.values;
-      });
-      color = ColorsWidget(
-        colors: values,
-        changeVariation: _changeVariation,
-      );
-    }
-    if (variation!.productPropertiesValues
-        .any((property) => property.property == 'Color')) {
-      String selectedColor = variation!.productPropertiesValues
-          .firstWhere((property) => property.property == 'Color')
-          .value;
-      List<String> sizesForSelectedColor = [];
-      if (variation!.productPropertiesValues
-          .any((property) => property.property == 'Size')) {
-        currentVariations.forEach((element) {
-          if (element.productPropertiesValues.any((property) =>
-                  property.property == 'Color' &&
-                  property.value == selectedColor) &&
-              element.productPropertiesValues
-                  .any((property) => property.property == 'Size')) {
-            element.productPropertiesValues
-                .where((property) => property.property == 'Size')
-                .forEach((property) {
-              sizesForSelectedColor.add(property.value);
-              print(sizesForSelectedColor);
-            });
-          }
-        });
 
-        size = SizeWidget(
-          values: sizesForSelectedColor,
-          changeVariation: _changeVariation,
-        );
-      }
-      if (variation!.productPropertiesValues
-          .any((property) => property.property == 'Materials')) {
-        List<String> materialsForSelectedColor = [];
-        currentVariations.forEach((element) {
-          if (element.productPropertiesValues.any((property) =>
-                  property.property == 'Color' &&
-                  property.value == selectedColor) &&
-              element.productPropertiesValues
-                  .any((property) => property.property == 'Materials')) {
-            element.productPropertiesValues
-                .where((property) => property.property == 'Materials')
-                .forEach((property) {
-              materialsForSelectedColor.add(property.value);
-              print(materialsForSelectedColor);
-            });
-          }
-        });
-        material = MaterialWidget(
-          materials: materialsForSelectedColor,
-          changeVariation: _changeVariation,
-        );
-      }
-    } else {
-      if (variation!.productPropertiesValues
-          .any((property) => property.property == 'Size')) {
-        List<String> values = [];
-        variation!.productPropertiesValues
-            .where((element) => element.property == 'Size')
+    List<String> colorValues = widget.product.availableProperties
+        .firstWhere((element) => element.property == 'Color')
+        .values;
+
+    return ColorsWidget(
+      colors: colorValues,
+      changeVariation: _changeVariation,
+    );
+  }
+
+  Widget _buildSizeWidget() {
+    if (variation == null ||
+        !variation!.productPropertiesValues
+            .any((property) => property.property == 'Size')) {
+      return Container();
+    }
+
+    String selectedColor = variation!.productPropertiesValues
+        .firstWhere(
+          (property) => property.property == 'Color',
+          orElse: () => ProductPropertyAndValue(property: 'Color', value: ''),
+        )
+        .value;
+    List<String> sizesForSelectedColor = [];
+
+    currentVariations.forEach((element) {
+      if (element.productPropertiesValues.any((property) =>
+              property.property == 'Color' &&
+              property.value == selectedColor) &&
+          element.productPropertiesValues
+              .any((property) => property.property == 'Size')) {
+        element.productPropertiesValues
+            .where((property) => property.property == 'Size')
             .forEach((property) {
-          values.add(property.value);
+          sizesForSelectedColor.add(property.value);
         });
-        material = MaterialWidget(
-          materials: values,
-          changeVariation: _changeVariation,
-        );
       }
-      if (variation!.productPropertiesValues
-          .any((property) => property.property == 'Materials')) {
-        List<String> values = [];
-        variation!.productPropertiesValues
-            .where((element) => element.property == 'Materials')
+    });
+
+    return SizeWidget(
+      values: sizesForSelectedColor,
+      changeVariation: _changeVariation,
+    );
+  }
+
+  Widget _buildMaterialWidget() {
+    if (variation == null ||
+        !variation!.productPropertiesValues
+            .any((property) => property.property == 'Materials')) {
+      return Container();
+    }
+    String selectedColor = variation!.productPropertiesValues
+        .firstWhere(
+          (property) => property.property == 'Color',
+          orElse: () => ProductPropertyAndValue(property: 'Color', value: ''),
+        )
+        .value;
+
+    List<String> materialsForSelectedColor = [];
+
+    currentVariations.forEach((element) {
+      if (element.productPropertiesValues.any((property) =>
+              property.property == 'Color' &&
+              property.value == selectedColor) &&
+          element.productPropertiesValues
+              .any((property) => property.property == 'Materials')) {
+        element.productPropertiesValues
+            .where((property) => property.property == 'Materials')
             .forEach((property) {
-          values.add(property.value);
+          materialsForSelectedColor.add(property.value);
         });
-        material = MaterialWidget(
-          materials: values,
-          changeVariation: _changeVariation,
-        );
       }
-    }
+    });
 
-    /*
-    if (variation!.productPropertiesValues
-        .any((property) => property.property == 'Materials')) {
-      List<String> values = [];
-      variation!.productPropertiesValues
-          .where((element) => element.property == 'Materials')
-          .forEach((property) {
-        values.add(property.value);
-      });
-      material = MaterialWidget(
-        materials: values,
-        changeVariation: _changeVariation,
-      );
-    }
-    */
+    return MaterialWidget(
+      materials: materialsForSelectedColor,
+      changeVariation: _changeVariation,
+    );
+  }
 
+  Widget _buildVariations() {
     return Column(
       children: [
         const SizedBox(
@@ -185,15 +182,15 @@ class _ProductDetailsScreenState extends State<ProductDetailsScreen> {
             const SizedBox(
               height: 20,
             ),
-            color ?? Container(),
+            _buildColorsWidget(),
             const SizedBox(
               height: 12,
             ),
-            size ?? Container(),
+            _buildSizeWidget(),
             const SizedBox(
               height: 12,
             ),
-            material ?? Container(),
+            _buildMaterialWidget(),
           ],
         ),
       ],
@@ -209,40 +206,75 @@ class _ProductDetailsScreenState extends State<ProductDetailsScreen> {
       for (var image in variation!.productVarientImages) {
         imagePaths.add(image.imagePath);
       }
-
       return imagePaths;
     }
-
     return [];
   }
 
-  Future<void> fetchProductVariations() async {
-    var url = Uri.https(
-        'slash-backend.onrender.com', '/product/${widget.product.id}');
-    // Await the http get response, then decode the json-formatted response.
-    var response = await http.get(url);
-    if (response.statusCode == 200) {
-      var jsonResponse =
-          convert.jsonDecode(response.body) as Map<String, dynamic>;
-      // Check if 'data' key exists and is a list
-      if (jsonResponse.containsKey('data')) {
-        List<dynamic> data = jsonResponse['data']['variations'];
-        List<dynamic> availableProperties =
-            jsonResponse['data']['avaiableProperties'];
+  Widget _buildImagePage() {
+    List<String> imagePaths = getImagePaths();
 
-        // Convert each element to a Product object
-        setState(() {
-          widget.product.variations =
-              data.map((item) => ProductVariation.fromJson(item)).toList();
-          widget.product.availableProperties = availableProperties
-              .map((property) => AvailableProperties.fromJson(property))
-              .toList();
-        });
-        print('succeeded');
-      } else {
-        print('Request failed with status: ${response.statusCode}.');
-      }
-    }
+    return Container(
+      height: 400,
+      width: double.infinity,
+      child: ImagePage(imagePath: imagePaths),
+    );
+  }
+
+  Widget _buildBrandInfo() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Text(widget.product.name),
+        ClipOval(
+          child: Image(
+            image: NetworkImage(widget.product.brandLogoUrl ?? ''),
+            height: 50,
+            width: 50,
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildPriceAndBrandName() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Text(
+          variation?.price.toString() ?? 'loading..',
+        ),
+        Text(widget.product.brandName ?? 'loading..'),
+      ],
+    );
+  }
+
+  Widget _buildDescription() {
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 8),
+      decoration: BoxDecoration(
+        color: Colors.white12,
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: ExpansionTile(
+        title: const Text('Description'),
+        children: [Text(widget.product.description)],
+      ),
+    );
+  }
+
+  Widget _buildAddToCartButton() {
+    return SizedBox(
+      width: double.infinity,
+      child: ElevatedButton.icon(
+        style: const ButtonStyle(
+          backgroundColor: MaterialStatePropertyAll(Colors.white12),
+        ),
+        onPressed: () {},
+        icon: const Icon(Icons.shopping_cart_checkout_rounded),
+        label: const Text('Add to Cart'),
+      ),
+    );
   }
 
   @override
@@ -256,55 +288,12 @@ class _ProductDetailsScreenState extends State<ProductDetailsScreen> {
           padding: const EdgeInsets.all(10.0),
           child: Column(
             children: [
-              Container(
-                height: 400,
-                width: double.infinity,
-                child: ImagePage(imagePath: getImagePaths()),
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(widget.product.name),
-                  ClipOval(
-                    child: Image(
-                      image: NetworkImage(widget.product.brandLogoUrl ?? ''),
-                      height: 50,
-                      width: 50,
-                    ),
-                  )
-                ],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    variation?.price.toString() ?? 'loading..',
-                  ),
-                  Text(widget.product.brandName ?? 'loading..'),
-                ],
-              ),
-              if (isDataInitialized) displayVariations() ?? Container(),
-              Container(
-                margin: EdgeInsets.symmetric(vertical: 8),
-                decoration: BoxDecoration(
-                  color: Colors.white12,
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                child: ExpansionTile(
-                  title: const Text('Description'),
-                  children: [Text(widget.product.description)],
-                ),
-              ),
-              SizedBox(
-                width: double.infinity,
-                child: ElevatedButton.icon(
-                    style: const ButtonStyle(
-                        backgroundColor:
-                            MaterialStatePropertyAll(Colors.white12)),
-                    onPressed: () {},
-                    icon: const Icon(Icons.shopping_cart_checkout_rounded),
-                    label: const Text('Add to Cart')),
-              )
+              _buildImagePage(),
+              _buildBrandInfo(),
+              _buildPriceAndBrandName(),
+              if (isDataInitialized) _buildVariations(),
+              _buildDescription(),
+              _buildAddToCartButton(),
             ],
           ),
         ),
